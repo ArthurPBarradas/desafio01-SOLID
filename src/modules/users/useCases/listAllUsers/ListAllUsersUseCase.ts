25b0a122aaf14eddf1b0d0a1f3d3468a1465142d
Arthur P. Barradas <101540115+ArthurPBarradas@users.noreply.github.com>
2023-01-09 18:49:30 -0300

    Add files via upload

---
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -0,0 +1,24 @@
+import { User } from "../../model/User";
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+interface IRequest {
+  user_id: string;
+}
+
+class ListAllUsersUseCase {
+  constructor(private usersRepository: IUsersRepository) {}
+
+  execute({ user_id }: IRequest): User[] {
+    const checkAdmin  = this.usersRepository.findById(user_id)
+
+    if (checkAdmin.admin === false) {
+      throw new Error("User not admin");
+    }
+
+    const users = this.usersRepository.list();
+
+    return users;
+  }
+}
+
+export { ListAllUsersUseCase };
